fd86fb438737a9c4e73a5baa5dbdbfb481e164f7
jondcoleman <jcoleman3307@gmail.com>
2016-07-07 17:50:04 -0400

    implementing agenda

--- server.js
+++ server.js
@@ -3,39 +3,38 @@
 var twilio = require('express')
 var express = require('express')
 var bodyParser = require('body-parser')
-var chrono = require('chrono-node')
-var CronJob = require('cron').CronJob
 var utils =  require('./utils')
+var Agenda = require('agenda')
 
 require('dotenv').config()
 
 var accountSid = process.env.ACCOUNT_SID // Your Account SID from www.twilio.com/console
 var authToken = process.env.AUTH_TOKEN   // Your Auth Token from www.twilio.com/console
 var appHost = process.env.APP_HOST
+var mongoConnectionString = process.env.MONGODB_URI
+var twilioNum = process.env.TWILIO_NUM
 var twilio = require('twilio');
 var client = new twilio.RestClient(accountSid, authToken);
+var agenda = new Agenda({db: {address: mongoConnectionString, collection: 'reminders'}});
 
-function createReminder(date, task) {
-  new CronJob(date, () => {
-    client.messages.create({
-        body: task,
-        to: '+13017755903',  // Text this number
-        from: '+12408984430' // From a valid Twilio number
-    }, function(err, message) {
-        if (err) console.log(err)
-        console.log(message.sid)
-    })
-  }, null, true, 'America/New_York')
-
-}
-client.messages.create({
-    body: 'Hello from Node',
-    to: '+13017755903',  // Text this number
-    from: '+12408984430' // From a valid Twilio number
-}, function(err, message) {
-    if (err) console.log(err)
-    console.log(message.sid)
+agenda.define('create scheduled reminder', function(job) {
+  client.messages.create({
+      body: job.attrs.data.task,
+      to: job.attrs.data.task,  // Text this number
+      from: twilioNum // From a valid Twilio number
+  }, function(err, message) {
+      if (err) console.log(err)
+      console.log(message.sid)
+  })
 })
+
+agenda.on('ready', function() {
+  agenda.start();
+});
+
+function createReminder(date, task, number) {
+  agenda.schedule(date, 'create scheduled reminder', {task, number})
+}
 
 var app = express();
 
@@ -49,17 +48,23 @@ app.get('/', (req, res) => {
 
 // Create a route to respond to a call
 app.post('/createReminder', twilio.webhook(authToken, {url: `${appHost}/createReminder`}), function(req, res) {
-  console.log(JSON.stringify(req.body.Body))
+  console.log(JSON.stringify(req.body))
 
   var twiml = new twilio.TwimlResponse();
   var msg = req.body.Body.toLowerCase()
-  // remind tomorrow to pick up laundry
+  var num = req.body.From
+  // MSG FORMAT: remind tomorrow to pick up laundry
+
   if (msg.split(' ')[0] === 'remind') {
     var task = msg.split(' to ')[1]
     var timeString = msg.split(' to ')[0].replace(/remind\s/, '')
+
     console.log(task, timeString)
+
     var time = utils.getDateFromString(timeString)
-    createReminder(time, task)
+
+    createReminder(time, task, num)
+
     twiml.message(`Reminder set to ${task} at ${time.toString()}!`)
   } else {
     twiml.message('No valid command!')
